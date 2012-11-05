4a2689900ec0c3ad04fa1f20830e87432f1a3e25
Frederick <fmichielssen@gmail.com>
2012-11-05 19:47:11 +0100

    Switched to Eclipse 3.8.1, R 2.15.2, Statet 3.1.2

--- eu.openanalytics.architect.r.libs.rj.win32.win32/libs/rj/jri/run.bat
+++ eu.openanalytics.architect.r.libs.rj.win32.win32/libs/rj/jri/run.bat
@@ -1,2 +1,2 @@
-set PATH=%PATH%;d:/dev/tools/R-214~1.2\bin;d:/dev/tools/R-214~1.2\lib
-d:/dev/tools/jdk/x64/JDK17~1.0/bin/java -cp .;examples;JRI.jar;src/JRI.jar rtest $*
+set PATH=%PATH%;c:/ARCHIT~1/DEPEND~1/R/target/R-215~1.2\bin;c:/ARCHIT~1/DEPEND~1/R/target/R-215~1.2\lib
+c:/ARCHIT~1/DEPEND~1/jdk/x64/JDK17~1.0/bin/java -cp .;examples;JRI.jar;src/JRI.jar rtest $*
