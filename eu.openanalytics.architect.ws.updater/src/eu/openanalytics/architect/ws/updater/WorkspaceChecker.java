39b178fe1722ecb3d436869bf80cb75d03f23df1
Frederick <fmichielssen@gmail.com>
2012-12-09 15:41:28 +0100

    Added platform-specific checks to workspace updater plugin.

--- eu.openanalytics.architect.ws.updater/src/eu/openanalytics/architect/ws/updater/WorkspaceChecker.java
+++ eu.openanalytics.architect.ws.updater/src/eu/openanalytics/architect/ws/updater/WorkspaceChecker.java
@@ -1,13 +1,21 @@
 package eu.openanalytics.architect.ws.updater;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
 
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Platform;
 import org.eclipse.core.runtime.Status;
 import org.osgi.framework.Version;
 
 import eu.openanalytics.architect.ws.updater.util.Logger;
+import eu.openanalytics.architect.ws.updater.util.StreamUtils;
 
 public class WorkspaceChecker {
 
@@ -24,7 +32,8 @@ public class WorkspaceChecker {
 			
 			String wsVersionString = wsAccessor.getWorkspaceVersion();
 			Version wsVersion = Version.parseVersion(wsVersionString);
-			Version latestVersion = Activator.getDefault().getBundle().getVersion();
+			String latestVersionString = getPluginWorkspaceVersion();
+			Version latestVersion = Version.parseVersion(latestVersionString);
 			boolean updateRequired = (latestVersion.compareTo(wsVersion) > 0);
 			
 			if (updateRequired) {
@@ -33,7 +42,12 @@ public class WorkspaceChecker {
 				if (monitor.isCanceled()) return Status.CANCEL_STATUS;
 				
 				monitor.subTask("Installing new workspace resources");
-				wsAccessor.replaceWorkspaceFolder(WorkspaceConstants.PATH_TO_COPY_FROM, "/");
+				wsAccessor.replaceWorkspaceFolder(WorkspaceConstants.DEFAULT_PLUGIN_WS_PATH, "/");
+				
+				String[] extras = getPlatformSpecificWorkspaces();
+				for (String extra: extras) {
+					wsAccessor.replaceWorkspaceFolder(extra, "/");
+				}
 				
 				monitor.subTask("Setting new workspace version");
 				wsAccessor.setWorkspaceVersion(latestVersion.toString());
@@ -48,5 +62,45 @@ public class WorkspaceChecker {
 		
 		monitor.done();
 		return Status.OK_STATUS;
+	}
+	
+	/*
+	 * Non-public
+	 * **********
+	 */
+	
+	private String getPluginWorkspaceVersion() throws IOException {
+		URL entry = Activator.getDefault().getBundle().getEntry("/workspace/" + WorkspaceConstants.VERSIONFILE_NAME);
+		if (entry == null) return null;
+		InputStream input = entry.openStream();
+		ByteArrayOutputStream output = new ByteArrayOutputStream();
+		StreamUtils.copyAndClose(input, output);
+		return output.toString();
+	}
+	
+	private String[] getPlatformSpecificWorkspaces() {
+		
+		List<String> components = new ArrayList<String>();
+		
+		String os = Platform.getOS();
+		String ws = Platform.getWS();
+		String arch = Platform.getOSArch();
+		
+		addPlatformSpecificWorkspaces(components, ws);
+		addPlatformSpecificWorkspaces(components, ws + "." + os);
+		addPlatformSpecificWorkspaces(components, ws + "." + os + "." + arch);
+		
+		return components.toArray(new String[components.size()]);
+	}
+	
+	private void addPlatformSpecificWorkspaces(List<String> components, String wsName) {
+		String searchPath = "/workspace/";
+		Enumeration<URL> entries = Activator.getDefault().getBundle().findEntries(searchPath, wsName, false);
+		while (entries != null && entries.hasMoreElements()) {
+			URL entry = entries.nextElement();
+			String entryString = entry.toString();
+			boolean isFolder = entryString.endsWith("/");
+			if (isFolder) components.add(searchPath + wsName + "/" + WorkspaceConstants.WS_PLUGINS_PATH);
+		}
 	}
 }
--- eu.openanalytics.architect.ws.updater/src/eu/openanalytics/architect/ws/updater/WorkspaceConstants.java
+++ eu.openanalytics.architect.ws.updater/src/eu/openanalytics/architect/ws/updater/WorkspaceConstants.java
@@ -3,17 +3,22 @@ package eu.openanalytics.architect.ws.updater;
 public class WorkspaceConstants {
 
 	/**
-	 * The path inside the workspace containing the per-plugin settings.
+	 * The name of the file containing the workspace version.
+	 */
+	public final static String VERSIONFILE_NAME = "ws.version.info";
+	
+	/**
+	 * The path inside the workspace containing the plugin settings.
 	 */
 	public final static String WS_PLUGINS_PATH = ".metadata/.plugins";
-	
+
 	/**
 	 * The path inside the workspace referring to the version file.
 	 */
-	public final static String WS_VERSIONFILE_PATH = WS_PLUGINS_PATH + "/" + Activator.PLUGIN_ID + "/ws.version.info";
+	public final static String WS_VERSIONFILE_PATH = WS_PLUGINS_PATH + "/" + Activator.PLUGIN_ID + "/" + VERSIONFILE_NAME;
 	
 	/**
 	 * The path inside this plugin containing up-to-date workspace files.
 	 */
-	public final static String PATH_TO_COPY_FROM = "/workspace/" + WS_PLUGINS_PATH;
+	public final static String DEFAULT_PLUGIN_WS_PATH = "/workspace/default/" + WS_PLUGINS_PATH;
 }
