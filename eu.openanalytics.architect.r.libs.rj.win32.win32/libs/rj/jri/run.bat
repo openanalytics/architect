905b734c683169c01fa15706f1d12d7487fedbd6
Frederick Michielssen <frederick.michielssen@openanalytics.eu>
2016-04-08 12:46:38 +0200

    Refactoring from PDE build to Tycho build (work in progress)

--- eu.openanalytics.architect.r.libs.rj.win32.win32/libs/rj/jri/run.bat
+++
@@ -1,2 +0,0 @@
-set PATH=%PATH%;c:/ARCHIT~1/DEPEND~1/R/target/R-215~1.2\bin;c:/ARCHIT~1/DEPEND~1/R/target/R-215~1.2\lib
-c:/ARCHIT~1/DEPEND~1/jdk/x64/JDK17~1.0/bin/java -cp .;examples;JRI.jar;src/JRI.jar rtest $*
--- eu.openanalytics.architect.r.server.win32.win32/R/library/MASS/tests/regression.R
+++
@@ -1,9 +0,0 @@
-### regression tests
-
-library(MASS)
-
-## uses Matrix
-contr.sdif(6)
-contr.sdif(6, sparse=TRUE)
-stopifnot(all(contr.sdif(6) == contr.sdif(6, sparse=TRUE)))
-
